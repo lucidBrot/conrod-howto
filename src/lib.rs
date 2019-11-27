cd18f075e46a6aff1aa2fc12dcde65f8e704f575
U-FRIENDLY_NAME\eric <eric@mink.li>
2019-11-27 16:02:16 +0100

    remove greenscreen at startup

--- src/lib.rs
+++ src/lib.rs
@@ -148,7 +148,7 @@ pub fn main() {
         if let Some(primitives) = ui.draw_if_changed() {
             renderer.fill(&display, primitives, &image_map);
             let mut target = display.draw();
-            target.clear_color(0.0, 1.0, 0.0, 1.0);
+            ////target.clear_color(0.0, 1.0, 0.0, 1.0);
             renderer.draw(&display, &mut target, &image_map).unwrap();
             target.finish().unwrap();
         }
